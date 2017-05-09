48246086523ec3e97aa0ff8558d2f78639704b78
double-underscore-get <double.underscore.get@gmail.com>
2017-05-09 14:11:19 +0200

    Fixed a3q2

--- a3q2.py
+++ a3q2.py
@@ -1,32 +1,26 @@
+from random import randint, random
 import turtle as t
-import moves as mov
-import random as rand
+from moves import request_moves
 # import the modules moves, and random here.
 
 t.speed(0)
 t.title('Monster Movement Pattern')
 t.up()
 
-
 pixels_per_move = 40
 
 # generate a random integer between 5 and 10 here (hint: randint() function in 'random' module) and
 # assign number_of_segments to refer to it.
-number_of_segments = rand.randint(5, 10)
+number_of_segments = randint(5, 10)
 
 # call the request_moves() function in the moves module with number_of_segments as the argument and
 # assign steps refer to the returned string.
-steps = mov.request_moves(number_of_segments)
-
+steps = request_moves(number_of_segments)
 
 # x and y store the coordinates at which the most recent circle was drawn:
 x = 0
 y = 0
-colorU = (rand.random(), rand.random(), rand.random())
-colorD = (rand.random(), rand.random(), rand.random())
-colorL = (rand.random(), rand.random(), rand.random())
-colorR = (rand.random(), rand.random(), rand.random())
-
+t.fillcolor((random(), random(), random()))
 # This following loop will draw all of the circles.
 # remember that i contains the current move number and the i-th character
 # of the string steps describes the current move.
@@ -44,23 +38,17 @@ colorR = (rand.random(), rand.random(), rand.random())
     # circle drawn is above the previous one.
     #<write code yere>
 
-for i in steps:
+for pos, i in enumerate(steps):
+    if pos != 0 and steps[pos-1] != i:
+        t.fillcolor((random(), random(), random()))
     if i == 'u':
         y += pixels_per_move
-        t.fillcolor(colorU)
-        if i == 'u' and t.color() != colorU:
-            y += pixels_per_move
-            colorU = (rand.random(), rand.random(), rand.random())
-            t.fillcolor(colorU)
     elif i == 'd':
         y -= pixels_per_move
-        t.fillcolor(colorD)
     elif i == 'r':
         x += pixels_per_move
-        t.fillcolor(colorR)
     elif i == 'l':
         x -= pixels_per_move
-        t.fillcolor(colorL)
 
 
     # draw a filled circle at location (x,y) with radius 15.  You'll need to use
